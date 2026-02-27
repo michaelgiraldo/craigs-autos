5132a4bcccbd2c4deebc40a290e6d007b3cc20ba
Michael Giraldo <michael.giraldo@gmail.com>
2026-02-26 20:58:32 -0800

    Add Korean commercial fleet upholstery page

--- src/lib/site-data.js
+++ src/lib/site-data.js
@@ -1836,6 +1836,7 @@ const COMMERCIAL_FLEET_PAGE_PATHS = {
 	fa: '/fa/روکش-ناوگان-تجاری/',
 	te: '/te/వాణిజ్య-ఫ్లీట్-అప్హోల్స్టరీ/',
 	fr: '/fr/garnissage-flotte-commerciale/',
+	ko: '/ko/상업용-플릿-내장/',
 };
 for (const [locale, mappedPath] of Object.entries(COMMERCIAL_FLEET_PAGE_PATHS)) {
 	PAGE_PATHS.commercialFleet = { ...(PAGE_PATHS.commercialFleet ?? {}), [locale]: mappedPath };
@@ -1850,6 +1851,7 @@ const COMMERCIAL_FLEET_NAV_LABELS = {
 	fa: 'ناوگان تجاری',
 	te: 'వాణిజ్య ఫ్లీట్',
 	fr: 'Flottes commerciales',
+	ko: '상업용 플릿',
 };
 for (const [locale, label] of Object.entries(COMMERCIAL_FLEET_NAV_LABELS)) {
 	NAV_LABELS[locale] = { ...(NAV_LABELS[locale] ?? {}), commercialFleet: label };
